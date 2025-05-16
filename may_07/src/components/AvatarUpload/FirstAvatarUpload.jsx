b59611ce61f367cc52eea9e60ea2e4e4c03cccb4
Oleksandr Nykytin <alex_n@alma-soft.com>
2025-05-16 18:51:02 +0300

    File Upload frontEnd

--- may_07/src/components/AvatarUpload/FirstAvatarUpload.jsx
+++ may_07/src/components/AvatarUpload/FirstAvatarUpload.jsx
@@ -1,12 +1,39 @@
+import {useState} from "react";
 
 export function FirstAvatarUpload() {
+
+    const [avatarUrl, setAvatarUrl] = useState(null);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        fetch('http://localhost:3000/upload', {
+            method: 'POST',
+            body: formData,
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            setAvatarUrl(data.url)
+            e.target.reset();
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    }
 
     return(<div>
         <h1>First Avatar Upload</h1>
         <p>Upload your first avatar here.</p>
-        <form action="http://localhost:3000/upload" method="post" encType="multipart/form-data">
+        <form onSubmit={onSubmit} encType="multipart/form-data">
             <input type="file" accept="image/*" name="file" />
             <input type="submit" value="Upload" />
         </form>
+        <div>
+            <div width="50%">
+                <h3> Server avatar </h3>
+                <img src={avatarUrl} alt="avatar" width="100px" />
+            </div>
+        </div>
     </div>)
 }
