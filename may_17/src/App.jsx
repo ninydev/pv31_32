a9292c0e8e8639286458d2eedc960caf5128527e
Oleksandr Nykytin <alex_n@alma-soft.com>
2025-05-17 16:47:28 +0300

    Good Router

--- may_17/src/App.jsx
+++ may_17/src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {BadRouter} from "./BadRouter/BadRouter.jsx";
+import {BadRouter} from "./MyRouter/BadRouter.jsx";
+import {GoodRouter} from "./MyRouter/GoodRouter.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-        <BadRouter />
+        <GoodRouter />
     </>
   )
 }
---
+++ may_17/src/MyRouter/GoodRouter.jsx
@@ -0,0 +1,30 @@
+import {Link, Route, BrowserRouter as Router, Routes} from "react-router";
+import {HomePage} from "../pages/HomePage.jsx";
+import {AboutPage} from "../pages/AboutPage.jsx";
+import {ContactPage} from "../pages/ContactPage.jsx";
+import {Error404Page} from "../pages/Error404Page.jsx";
+
+export function GoodRouter() {
+    return (
+        <>
+            <div>
+                <Router>
+
+                    <div>
+                        <h1> Good Router </h1>
+                        <Link to="/">Home</Link>
+                        <Link to="/about">About</Link>
+                        <Link to="/contact">Contact</Link>
+                    </div>
+
+                    <Routes>
+                        <Route path="/" element={<HomePage/>}/>
+                        <Route path="/about" element={<AboutPage/>}/>
+                        <Route path="/contact" element={<ContactPage/>}/>
+                        <Route path="*" element={<Error404Page/>}/>
+                    </Routes>
+                </Router>
+            </div>
+        </>
+    )
+}
