35d09c67ed76185c670cba2d0fed0d28dd4150d1
Oleksandr Nykytin <alex_n@alma-soft.com>
2025-06-30 20:50:12 +0300

    30 june 2025

---
+++ WebApplicationAuth/WebApplicationAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebApplicationAuth.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
